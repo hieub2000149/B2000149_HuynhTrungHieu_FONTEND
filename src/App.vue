4d5ef83de050864efad3e21774013b0a623d788c
hieub2000149 <hieub2000149@student.ctu.edu.vn>
2024-10-31 16:37:58 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
-<!-- <script>
-export default {
-}
-</script>
-<template>
-<h1>Hello, Vue.js!</h1>
-</template>
-<style>
-.page {
-max-width: 400px;
-margin: auto;
-}
-</style> -->
-
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 export default {
-components: {
-AppHeader,
-},
+    components: {
+        AppHeader,
+    },
 };
 </script>
 <template>
-<div id="app">
-<AppHeader />
-<div class="container mt-3">
-<router-view />
-</div>
-</div>
+    <div id="app">
+        <AppHeader />
+        <div class="container mt-3">
+            <router-view />
+        </div>
+    </div>
 </template>
 <style>
 .page {
-max-width: 400px;
-margin: auto;
+    max-width: 400px;
+    margin: auto;
 }
 </style>
